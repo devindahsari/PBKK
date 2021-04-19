a8beae196b868274b5a56d5c2bd072999e86ac19
devindahsari <38748307+devindahsari@users.noreply.github.com>
2021-04-19 14:55:43 +0700

    Add files via upload

---
+++ HelloWorld/HelloWorld/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            setLabel2();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void setLabel2()
+        {
+            label2.Text = "Devi Indah Sari";
+            label2.Font = new Font("Comic Sans MS", 12, FontStyle.Underline);
+        }
+    }
+}
